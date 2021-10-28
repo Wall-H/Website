f94cc516744c643a7671b1787706df7e77c5e58c
Wall-H <wallhrobot@gmail.com>
2021-10-28 14:49:58 -0300

    Update ligthboxt.js

---
+++ js/ligthboxt.js
@@ -0,0 +1,6 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+
+
+imagenes.forEach(imagen 0 > )
